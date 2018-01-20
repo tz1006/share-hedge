b67b66258fc577aef5ae35942ef4a7dd33856237
tz1006 <a@omg.tf>
2018-01-20 05:09:08 -0800

    Create pair.py

---
+++ pair.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*- 
+# filename: pair.py
+
+from tools import *
+from sharelist_t import share_list
+import threading
+
+def pair(code1, code2):
+    r = contrast(code1,code2)
+    print(code1, code2)
+    if r[1] > 80:
+        d['%s-%s' % (code1,code2)] = r[0]
+
+
+def test():
+    global d
+    d = {}
+    li = []
+    for i in share_list:
+        for l in share_list:
+            if i != l:
+                if int(i) < int(l):
+                    text = '%s-%s' % (i,l)
+                    if text not in li:
+                        li.append(text)
+                        pair(i,l)
+                else:
+                    text = '%s-%s' % (l,i)
+                    if text not in li:
+                        li.append(text)
+                        pair(l,i)
+    #d = sorted(d.items(), key=lambda d:d[0])
+    print('Finish')
+
+#test()
+
+
+def test_t():
+    global d
+    d = {}
+    li = []
+    threads = []
+    for i in share_list:
+        for l in share_list:
+            if i != l:
+                if int(i) < int(l):
+                    text = '%s-%s' % (i,l)
+                    if text not in li:
+                        li.append(text)
+                        a = threading.Thread(target=pair, args=(i,l))
+                        threads.append(a)
+                        a.start()
+                else:
+                    text = '%s-%s' % (l,i)
+                    if text not in li:
+                        li.append(text)
+                        a = threading.Thread(target=pair, args=(i,l))
+                        threads.append(a)
+                        a.start()
+    for t in threads:
+        t.join()
+    d = sorted(d.items(), key=lambda d:d[0])
+    print('Finish')
+
+
+import code
+code.interact(banner = "", local = locals())
+
