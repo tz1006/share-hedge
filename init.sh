a118206b404f742fe8a3193db3dce07aa3eb2ffc
tz1006 <a@omg.tf>
2018-01-21 05:32:06 -0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ sudo apt-get update -y
 sudo apt-get install -y python3-pip pypy
 sudo pip3 install --upgrade pip
 sudo pip3 install pytz BeautifulSoup4
-pypy -m pip install pytz
+pypy -m pip install pytz BeautifulSoup4 requests
